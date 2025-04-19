e374184d21c37b21694a9a606d09638a87405a47
Ronnie Samuel <samuelronnie55@gmail.com>
2025-04-19 01:03:43 -0400

    Added and Edited files

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-    //
+    //@return \Illuminate\Http\JsonResponse
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+        if (! $token = auth::guard()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        return response()->json([
+            'message' => 'Login successful',
+            'access_token' => $token,
+        ]);
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return response()->json(['message' => 'Successfully logged successful']);
+    }
 }
